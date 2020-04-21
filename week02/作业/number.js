e2d07c8abfa3098402d56518c9653b92e364d425
ghj <1966778960@qq.com>
2020-04-22 00:00:50 +0800

    feat: string字面量相关代码

--- week02/作业/number.js
+++ week02/作业/number.js
@@ -19,10 +19,12 @@ const DecimalDigit = /[0-9]/
 const DecimalDigits = /[0-9]+/ //DecimalDigits :: DecimalDigit |DecimalDigits DecimalDigit 
 const DecimalIntegerLiteral = /(0|[1-9][0-9]*)/
 // const DecimalLiteral = /(DecimalIntegerLiteral.DecimalDigits* ExponentPart* |. DecimalDigits ExponentPart*|DecimalIntegerLiteral ExponentPart*)/
-const DecimalLiteral = /((0|[1-9][0-9]*).[0-9]* ([eE][+-]*[0-9]+)* |.[0-9]+([eE][+-]*[0-9]+)*|(0|[1-9][0-9]*)([eE][+-]*[0-9]+)*)/
+const DecimalLiteral = /((0|[1-9][0-9]*).[0-9]*([eE][+-]*[0-9]+)*|.[0-9]+([eE][+-]*[0-9]+)*|(0|[1-9][0-9]*)([eE][+-]*[0-9]+)*)/
 
 // const NumericLiteral = /(DecimalLiteral|HexIntegerLiteral)/
-const NumericLiteral = /(((0|[1-9][0-9]*).[0-9]* ([eE][+-]*[0-9]+)* |.[0-9]+([eE][+-]*[0-9]+)*|(0|[1-9][0-9]*)([eE][+-]*[0-9]+)*)|0[xX][0-9A-Fa-f]+)/
+const NumericLiteral = /((^((0|[1-9][0-9]*).[0-9]*([eE][+-]*[0-9]+)*)$|^(.[0-9]+([eE][+-]*[0-9]+)*)$|^((0|[1-9][0-9]*)([eE][+-]*[0-9]+)*)$)|^0[xX][0-9A-Fa-f]+$)/
 
 console.log(NumericLiteral.test(0x78A))
 console.log(NumericLiteral.test(.6))
+console.log(NumericLiteral.test('00.6'))
+console.log(NumericLiteral.test('0gg.6'))
--- week02/作业/string.js
+++ week02/作业/string.js
@@ -10,17 +10,33 @@ const SingleStringCharacters = /SingleStringCharacter SingleStringCharactersopt/
 
 const DoubleStringCharacter = /(SourceCharacter but not one of " or \ or LineTerminator|\ EscapeSequence|LineContinuation)/
 const SingleStringCharacter = /(SourceCharacter but not one of ' or \ or LineTerminator |\ EscapeSequence|LineContinuation)/
+// <LF>(\u000A) <CR>(\u000D) [lookahead  <LF> ] <LS>(\u2028) <PS>(\u2029) <CR> <LF> 
+const LineContinuation = /[\\]n/
+// console.log(LineContinuation.test('\n'))
 
-const LineContinuation = /\ LineTerminatorSequence/
+// const EscapeSequence = /(CharacterEscapeSequence|HexEscapeSequence|UnicodeEscapeSequence|0  [lookahead  DecimalDigit])/
+const EscapeSequence = /(CharacterEscapeSequence|x[0-9A-Fa-f]{2}|u[0-9A-Fa-f]{4}|0[lookahead  DecimalDigit])/
 
-const EscapeSequence = /(CharacterEscapeSequence|HexEscapeSequence|UnicodeEscapeSequence|0  [lookahead  DecimalDigit])/
-
-const CharacterEscapeSequence = /(SingleEscapeCharacter|NonEscapeCharacter)/
+const CharacterEscapeSequence = /(^['"\\bfnrtv]$|NonEscapeCharacter)/
 
 const SingleEscapeCharacter = /['"\\bfnrtv]/
+// console.log(SingleEscapeCharacter.test("'"))
+// console.log(SingleEscapeCharacter.test('\\'))
+// console.log(SingleEscapeCharacter.test('b'))
+// console.log(SingleEscapeCharacter.test('f'))
+// console.log(SingleEscapeCharacter.test('n'))
 
-const NonEscapeCharacter = 'SourceCharacter but not one of EscapeCharacter or LineTerminator'
-const EscapeCharacter = /(SingleEscapeCharacter|DecimalDigit|x|u)/
-const HexEscapeSequence = /x HexDigit{2}/
 
-const UnicodeEscapeSequence = /u HexDigit{4}/
+// LineTerminator :: <LF>(n) <CR>(r) <LS> <PS> 
+// const NonEscapeCharacter = 'SourceCharacter but not one of EscapeCharacter or LineTerminator'
+const NonEscapeCharacter = /[^'"\\bfnrtv]/ //'SourceCharacter but not one of EscapeCharacter or LineTerminator'
+// console.log(NonEscapeCharacter.test('\\'))
+// console.log(NonEscapeCharacter.test('v'))
+console.log(NonEscapeCharacter.test('\u2028'))
+console.log(NonEscapeCharacter.test('\u2029'))
+
+// const EscapeCharacter = /(SingleEscapeCharacter|DecimalDigit|x|u)/
+const EscapeCharacter = /(^['"\\bfnrtv]$|^[0-9]$|x|u)/
+const HexEscapeSequence = /x[0-9A-Fa-f]{2}/
+
+const UnicodeEscapeSequence = /u[0-9A-Fa-f]{4}/
