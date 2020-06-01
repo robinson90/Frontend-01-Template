6a4c38b00697420a0a51a3b514054a49758ee31c
ghj <1966778960@qq.com>
2020-06-02 00:53:30 +0800

    feat: 测试

--- week08/homework/match.js
+++ week08/homework/match.js
@@ -154,5 +154,7 @@ function match(selector, element) {
   return true;
 }
 
-
+match("div #id.class+p~span", document.getElementById("id"));
 match("div div>#id.class", document.getElementById("id"));
+match("div div>#id.class+p", document.getElementById("id"));
+match("div div>#id.class+p~span", document.getElementById("id"));
